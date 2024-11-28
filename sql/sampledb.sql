1ef4e212edadabff8e05c3520d2bb90bcddbd066
UNOSHE0516 <hiro0516jms@gmail.com>
2024-11-28 20:51:00 +0900

    エラー出た

---
+++ sql/sampledb.sql
@@ -0,0 +1,11 @@
+create database sampledb;
+use sampledb;
+
+create table sample_table(
+No int,
+name varchar(50),
+age int,
+Mail varchar(50));
+insert into sampledb values(1,"Yamada",21,"xxx@yahoo.co.jp");
+insert into sampledb values(2,"Sato",39,"yyy@google.com");
+insert into sampledb values(3,"Harada",44,"zzz@yahoo.co.jp");
