b64b3bc01066ebbd6b32d44d8d5db1da1c178ce7
UNOSHE0516 <hiro0516jms@gmail.com>
2024-12-04 22:03:06 +0900

    ７のプログラム追加

--- src/TestUserDAO.java
+++ src/TestUserDAO.java
@@ -39,6 +39,7 @@ public class TestUserDAO {
 		} 
 	} 
 	
+//⑥文章追加
 	 public void selectAll() { 
 	     DBConnector db = new DBConnector(); 
 	     Connection con = db.getConnection(); 
@@ -61,6 +62,32 @@ public class TestUserDAO {
 		     } catch (SQLException e ) { 
 		       e.printStackTrace(); 
 		     } 
-	  } 
+	  }
+	 
+	//⑦文章追加
+	 public void selectByName(String name)
+	 { 
+		DBConnector db = new DBConnector(); 
+		Connection con = db.getConnection(); 
+		String sql ="select * from test_table where user_name=?"; 
+		try { 
+			PreparedStatement ps = con.prepareStatement(sql); 
+			ps.setString(1, name); 
+			ResultSet rs=ps.executeQuery(); 
+				while (rs.next()) { 
+					System.out.println(rs.getString("user_name")); 
+					System.out.println(rs.getString("password")); 
+				} 
+			} catch (SQLException e ) { 
+			e.printStackTrace(); 
+			} 
+		try { 
+			con.close() ; 
+		} catch (SQLException e ) { 
+			e.printStackTrace(); 
+		} 
+	} 
+
+	 
 	
 }
