2fa8be360c96041a5aefcf948be9f846e2ca84ff
UNOSHE0516 <hiro0516jms@gmail.com>
2024-12-04 21:24:12 +0900

    メモ書きを入れた

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -5,19 +5,24 @@ import java.sql.SQLException;
 public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
 	
+//	JDBC のドライバーの名前を変数に代入しています。ちなみにドライバーとはJavaとデータベースを繋げる工具箱の様な物です。 
 	private static String url =
 "jdbc:mysql://localhost/testdb";
 	
+//	rootアカウントに対するパスワードを”mysql”に指定しています。 
 	private static String user = "root"; 
 	private static String password = ""; 
 	
-//	１５、１６行目は公式として覚える
+//★18、20行目の二つは接続しかしないクラス。公式として覚えておく
+//	接続状態にする	
 	public Connection getConnection() { 
+//	一度状態を初期化している
 	Connection con = null; 
 	
-//	ドライバーがロードされ使えるような状態にしている。２０，２１行目は覚える
+//	
 	try{ 
 	Class.forName(driverName); 
+//★26行目ここではドライバーがロードされ使えるような状態にしている。覚える
 	con = DriverManager.getConnection(url,user,password); 
 	} catch (ClassNotFoundException e) { 
 	e.printStackTrace() ; 
--- src/TestUserDAO.java
+++ src/TestUserDAO.java
@@ -1,23 +1,29 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException; 
+import java.sql.SQLException;
+
 
 public class TestUserDAO {
 	String name = ""; 
 	String password = "";
 	public void select(String name,String password) {
 	
-//	１２、１３行目DBへの接続準備　これでmysqlにログインできる。
+//★13、14行目DBへの接続準備　これでmysqlにログインできる。
 	DBConnector db = new DBConnector(); 
 	Connection con = db.getConnection(); 
 	
 	String sql ="select * from test_table where user_name=? and password=?"; 
-	try { 
+	try {
+		
+//	PreparedStatementはDBまで運んでくれる箱
 	PreparedStatement ps = con.prepareStatement(sql); 
 	ps.setString(1, name); 
 	ps.setString (2, password); 
+	
+//★executeQuery(); は実行で必ずResultSetが返ってくる
 	ResultSet rs=ps.executeQuery(); 
+//	1行づつデータを抽出してくれる
 	if (rs.next()) { 
 	System.out.println(rs.getString("user_name")); 
 	System.out.println(rs.getString("password")); 
@@ -26,6 +32,7 @@ public class TestUserDAO {
 	e.printStackTrace(); 
 	} 
 	try { 
+//		36行目データベースの接続を終了させる。終わりには必ず入れる
 	con.close() ; 
 	} catch (SQLException e ) { 
 	e.printStackTrace(); 
