ee2e89da4c09208a71b2cffef182bb1e2586c05f
UNOSHE0516 <hiro0516jms@gmail.com>
2024-12-04 21:46:14 +0900

    TestUserDAOに追加記入

--- src/TestUserDAO.java
+++ src/TestUserDAO.java
@@ -10,33 +10,57 @@ public class TestUserDAO {
 	public void select(String name,String password) {
 	
 //★13、14行目DBへの接続準備　これでmysqlにログインできる。
-	DBConnector db = new DBConnector(); 
-	Connection con = db.getConnection(); 
-	
-	String sql ="select * from test_table where user_name=? and password=?"; 
-	try {
+		DBConnector db = new DBConnector(); 
+		Connection con = db.getConnection(); 
+		
+		String sql ="select * from test_table where user_name=? and password=?"; 
+		try {
 		
 //	PreparedStatementはDBまで運んでくれる箱
-	PreparedStatement ps = con.prepareStatement(sql); 
-	ps.setString(1, name); 
-	ps.setString (2, password); 
+		PreparedStatement ps = con.prepareStatement(sql); 
+		ps.setString(1, name); 
+		ps.setString (2, password); 
 	
 //★executeQuery(); は実行で必ずResultSetが返ってくる
-	ResultSet rs=ps.executeQuery(); 
+		ResultSet rs=ps.executeQuery(); 
 //	1行づつデータを抽出してくれる
-	if (rs.next()) { 
-	System.out.println(rs.getString("user_name")); 
-	System.out.println(rs.getString("password")); 
-	} 
-	} catch (SQLException e ) { 
-	e.printStackTrace(); 
-	} 
-	try { 
+		if (rs.next()) { 
+		System.out.println(rs.getString("user_name")); 
+		System.out.println(rs.getString("password")); 
+		} 
+		} catch (SQLException e ) { 
+		e.printStackTrace(); 
+		} 
+		try { 
 //		36行目データベースの接続を終了させる。終わりには必ず入れる
-	con.close() ; 
-	} catch (SQLException e ) { 
-	e.printStackTrace(); 
+		con.close() ; 
+		} catch (SQLException e ) { 
+		e.printStackTrace(); 
+		} 
 	} 
-	} 
+	
+	 public void selectAll() { 
+	     DBConnector db = new DBConnector(); 
+	     Connection con = db.getConnection(); 
+	 
+	     String sql ="select * from test_table"; 
+	     try { 
+	       PreparedStatement ps = con.prepareStatement(sql); 
+	       ResultSet rs=ps.executeQuery(); 
+	       
+//	       カーソルを１行ずつ実行していき、データがなくなったら実行を終了して下さいという意味。
+	       while (rs.next()) { 
+	         System.out.println(rs.getString("user_name")); 
+	         System.out.println(rs.getString("password")); 
+	       } 
+	     } catch (SQLException e ) { 
+	       e.printStackTrace(); 
+	     } 
+		 try { 
+		       con.close() ; 
+		     } catch (SQLException e ) { 
+		       e.printStackTrace(); 
+		     } 
+	  } 
 	
 }
